5074c169585b0c36e9f0c24dea1500ce576f96c5
benji <90022974@study.curtincollege.edu.au>
2023-03-13 20:35:07 +0800

    added modifications to message 4 - 5

--- Maze.py
+++ Maze.py
@@ -76,7 +76,7 @@ if __name__ == "__main__":
             elif grid[currentRow][currentCol] == END:
                 done = True
                 solved = True
-                print("MESSAGE 4") # Solved.
+                print("SOLVED!") # Solved.
                 
             else:
                 pass # Do nothing
@@ -86,7 +86,7 @@ if __name__ == "__main__":
 
 
     if not solved:
-        print("MESSAGE 5") # Did not reach the end.
+        print("you have failed to escape. Future archaeologists gaze upon your remains in bafflement") # Did not reach the end.
 
 
     GridViewer.view(grid)
