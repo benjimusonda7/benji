6ceb2ca3b649024d2f5095da639f5f40de060eb9
benji <90022974@study.curtincollege.edu.au>
2023-03-13 20:20:33 +0800

    added messages to 1 - 3.

--- Maze.py
+++ Maze.py
@@ -57,13 +57,13 @@ if __name__ == "__main__":
             currentCol -= 1
         
         else:
-            print("MESSAGE 1") # Invalid direction.
+            print("you have no idea where youre going") # Invalid direction.
         
         if (currentRow < 0 or currentCol < 0 
                         or currentRow >= len(grid) 
                         or currentCol >= len(grid[currentRow])):
             done = True
-            print("MESSAGE 2") # Out of bounds.
+            print("you fall into the chasm of doom") # Out of bounds.
             
         else:
             if grid[currentRow][currentCol] == EMPTY:
@@ -71,12 +71,12 @@ if __name__ == "__main__":
                 
             elif grid[currentRow][currentCol] == WALL:
                 done = True
-                print("MESSAGE 3") # Hit wall.
+                print("you stumble badly into a solid concrete wall") # Hit wall.
 
             elif grid[currentRow][currentCol] == END:
                 done = True
                 solved = True
-                print("MESSAGE 4") # Solved.
+                print("") # Solved.
                 
             else:
                 pass # Do nothing
@@ -86,7 +86,7 @@ if __name__ == "__main__":
 
 
     if not solved:
-        print("MESSAGE 5") # Did not reach the end.
+        print("") # Did not reach the end.
 
 
     GridViewer.view(grid)
